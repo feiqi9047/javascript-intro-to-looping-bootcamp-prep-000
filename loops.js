aaca097857057a30dc9f141e222fc4a072831b47
Fei Qi <feiqi9047@gmail.com>
2018-11-15 16:45:39 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -25,6 +25,6 @@ function doWhileLoop (array){
   
   do{ 
     array.pop();
-  } while (array.length > 0 && incrementVariable());
+  } while (array.length > -1 && incrementVariable());
 return array;
 }
